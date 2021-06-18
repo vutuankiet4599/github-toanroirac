1c4012bf058475bc67e0f43065e6ed376cc8e078
huylinh <bn@v_d.com>
2021-06-19 00:16:12 +0700

    Add them

---
+++ ThuvienToanRoiRac/graphlib.c
@@ -0,0 +1,230 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"graphlib.h"
+#include "string.h"
+graph creategraph(){
+  return make_jrb();
+}
+
+void addedge(graph graph, int v1, int v2)
+{
+  JRB node=jrb_find_int(graph,v1);
+  JRB tree;
+  if(node==NULL) {
+    tree=make_jrb();
+    jrb_insert_int(graph,v1,new_jval_v(tree));
+    jrb_insert_int(tree,v2,JNULL);
+  }
+  else {tree=(JRB)(node->val).v;
+    jrb_insert_int(tree,v2,JNULL);
+  }
+  node=jrb_find_int(graph,v2);
+  if(node==NULL) { 
+    tree=make_jrb();
+    jrb_insert_int(graph,v2,new_jval_v(tree));
+    jrb_insert_int(tree,v1,JNULL);
+  }
+  else {tree=(JRB)(node->val).v;
+    jrb_insert_int(tree,v1,JNULL);
+  }
+}
+//tra ve 0 neu khong tim thay ,1 tim thay
+int adjacent(graph graph, int v1, int v2)
+{
+  JRB tree,node=jrb_find_int(graph,v1);
+  if(node==NULL) return 0;
+  
+  tree=(JRB)(node->val).v;
+  if(jrb_find_int(tree,v2)==NULL) return 0;
+  return 1;
+}
+
+int incidentedges(graph graph, int vertex, int* output)
+{
+  int num=0;
+  JRB tree,node=jrb_find_int(graph,vertex);
+  if(node==NULL) return 0;
+  tree=(JRB)(node->val).v;
+  jrb_traverse(node,tree){
+    output[num++]=(node->key).i;
+  }
+  return num;
+}
+
+void dropgraph(graph graph)
+{
+  JRB node;
+  jrb_traverse(node,graph){
+    jrb_free_tree((JRB)jval_v(node->val));
+  }
+  jrb_free_tree(graph);
+}
+//prufercode
+void prtprufercode(int *bac,dscanh *ds1,int n){
+  int i,k;
+  if(bac[0]==0)return;
+  else{
+    for ( i = 1; i < n; i++)
+    {
+      //printf("a[%d]=%d ",i,bac[i]);
+      if(bac[i]==1){
+        for ( k = 0; k < n-1; k++)
+        {
+          if (ds1[k].x==i){
+            int z;
+            z=ds1[k].y;
+            if(z==0 && (bac[z]-1)==0)return;// khong in ra so 0 o cuoi cung
+          	printf("%d",ds1[k].y);
+          	ds1[k].x=-1;
+            ds1[k].y=-1;
+            bac[z]--;
+            bac[i]--;
+            break;
+		  }
+          if (ds1[k].y==i){
+             int z;
+             z=ds1[k].x;
+             if(z==0 && (bac[z]-1)==0)return;// khong in ra so 0 o cuoi cungs
+          	printf("%d",ds1[k].x);
+           
+          	ds1[k].x=-1;
+            ds1[k].y=-1;
+            bac[z]--;
+            bac[i]--;
+            break;
+		  }
+            //printf("canh %d %d",i,k);
+        }
+        break;
+      }
+    }
+  }
+  prtprufercode(bac,ds1,n);
+}
+graph readfromfile(char *filename,Ver ListVer[],int *sodinh,int *socanh){
+  graph g=creategraph();
+  int output[50];
+  FILE *f=fopen(filename,"r");
+  if(f==NULL){
+    printf("Loi mo file!");
+    exit(1);
+  }
+  fscanf(f,"%d %d\n",sodinh,socanh);
+  int i,canh1,canh2;
+  for ( i = 0; i < *socanh; i++)
+  {
+    fscanf(f,"%d %d\n",&canh1,&canh2);
+    //printf("check edge:%d--%d\n",canh1,canh2);
+    addedge(g,canh1,canh2);
+  }if(i==1){
+    fscanf(f,"%d %d",&canh1,&canh2);
+     //  printf("check edge:%d--%d\n",canh1,canh2);
+    addedge(g,canh1,canh2);
+  }
+  for ( i = 0; i < *sodinh; i++)
+  {
+    ListVer[i].idVer=i+1;//tu danh so dinh bat dau bang 1
+    ListVer[i].Bac=incidentedges(g,i+1,output);
+  }
+  
+  return g;
+}
+void sapxepbac(Ver ListVer[],int sodinh){
+  int i,j;
+  for ( i = 0; i < sodinh; i++)
+  {
+    for (j=i+1; j < sodinh; j++)
+    {
+      if(ListVer[i].Bac<ListVer[j].Bac){
+        Ver tmp=ListVer[i];
+        ListVer[i]=ListVer[j];
+        ListVer[j]=tmp;
+      }
+    }
+  }
+}
+void tomau(graph g,Ver ListVer[],int sodinh,int countmau){
+  ListVer[0].colorVer=0;
+  int i,j,colorOfVer;
+  int checkmau[countmau];
+  for ( i = 1; i < sodinh; i++)
+  {
+    memset(checkmau,0,sizeof(int)*countmau);//0 la chua danh ,1 la danh roi
+    for(j=0;j<i;j++){
+        if(adjacent(g,ListVer[i].idVer,ListVer[j].idVer)){
+          
+            colorOfVer=ListVer[j].colorVer;
+            checkmau[colorOfVer]=1;
+        }
+    }
+    for (j = 0; j < countmau; j++)
+    { 
+      if(checkmau[j]==0){
+        ListVer[i].colorVer=j;
+        break;
+      }
+    }
+  }
+}
+char *getnamecolor(Mau mau[],int countmau,int idmau){
+  int i;
+  for ( i = 0; i < countmau; i++)
+  {
+      if(mau[i].idcolor==idmau)return mau[i].color;
+  }
+  
+}
+void printdotfile(char *datafile,char *filenamedot,Ver ListVer[],Mau bangmau[],int countmau){
+  
+  FILE *f=fopen(filenamedot,"w+");
+  if(f==NULL){
+    printf("cannot write");
+    exit(1);
+  }
+  FILE *f2=fopen(datafile,"r");
+  if(f2==NULL){
+    printf("Loi mo file!");
+    exit(1);
+  }
+  int sodinh,socanh;
+  fscanf(f2,"%d %d\n",&sodinh,&socanh);
+  int i;
+  fprintf(f,"graph dothi{\n");
+  for ( i = 0; i < sodinh; i++)
+  {
+    fprintf(f,"%d [fillcolor=%s, style=filled];\n",ListVer[i].idVer,getnamecolor(bangmau,countmau,ListVer[i].colorVer));
+  }
+  
+  int canh1,canh2;
+  for ( i = 0; i < socanh; i++)
+  {
+    fscanf(f2,"%d %d\n",&canh1,&canh2);
+    //printf("check edge:%d--%d\n",canh1,canh2);
+    fprintf(f,"%d -- %d;\n",canh1,canh2);
+  }if(i==1){
+    fscanf(f2,"%d %d",&canh1,&canh2);
+    fprintf(f,"%d -- %d;\n",canh1,canh2);
+     //  printf("check edge:%d--%d\n",canh1,canh2);
+  }
+  fprintf(f,"}");
+  
+
+}
+void readfilePrufer(char *filename,dscanh ds1[],int bac[],int *n){
+  FILE *f=fopen(filename,"r");
+  if(f==NULL){
+    printf("Error open file data file prufercode");
+    exit(1);
+  }
+   fscanf(f,"%d\n",n);
+  int x,y,i;
+  for ( i = 0; i < *n; i++)
+  {
+    fscanf(f,"%d %d",&x,&y);
+    if(i!=*n-1)fscanf(f,"\n");
+    ds1[i].x=x;
+    ds1[i].y=y;
+    bac[x]++;
+    bac[y]++;
+  }
+}
--- ThuvienToanRoiRac/main.c
+++ ThuvienToanRoiRac/main.c
@@ -1,7 +1,8 @@
 #include "weightedGraphlib.h"
 #include <stdio.h>
 #include <stdbool.h>
-
+#include "graphlib.h"
+#include <string.h>
 void printVertex(int key)
 {
   printf("%4d", key);
@@ -58,8 +59,51 @@ int main(){
   printf("\n\n");
   printf("total = %f\n", total);
   printf("\n");
-  printf("Topo sort:\n");
-  topology(g, printVertex);
-  printf("\n");
+  //kiem tra DAG
+  if(DAG(g)){
+      printf("Topo sort:\n");
+      topology(g, printVertex);
+      printf("\n");
+  }else printf("Khong co sap xep topology");
   dropGraph(g);
+  //Do thi vo huong
+  int sodinh,socanh;
+  graph g2 = creategraph();
+  Ver ListVer[50];
+  Mau bangmau[20];
+  //su dung max k+1 mau;tuc la max la so dinh tuong duong moi dinh to 1 mau
+  //cho san bang mau voi 8 mau
+  int countmau=8;
+  bangmau[0].idcolor=0;
+  bangmau[1].idcolor=1;
+  bangmau[2].idcolor=2;
+  bangmau[3].idcolor=3;
+  bangmau[4].idcolor=4;
+  bangmau[5].idcolor=5;
+  bangmau[6].idcolor=6;
+  bangmau[7].idcolor=7;
+  //tuy vao so dinh ta dat duoc cac gia tri cho bang mau, va dat san mau` cho tung id id se dien sau theo so dinh
+  strcpy(bangmau[0].color,"red");
+  strcpy(bangmau[1].color,"lightseagreen");
+  strcpy(bangmau[2].color,"yellow");
+  strcpy(bangmau[3].color,"brown");
+  strcpy(bangmau[4].color,"green");
+  strcpy(bangmau[5].color,"cyan");
+  strcpy(bangmau[6].color,"whitesmoke");
+  strcpy(bangmau[7].color,"blueviolet");
+  g2=readfromfile("dothi.txt",ListVer,&sodinh,&socanh);
+  sapxepbac(ListVer,sodinh);
+  tomau(g2,ListVer,sodinh,countmau);
+  //printf("%d %d ->%d\n",ListVer[0].idVer,ListVer[1].idVer,adjacent(g,ListVer[0].idVer,ListVer[1].idVer));
+  printdotfile("dothi.txt","dothitomau.dot",ListVer,bangmau,countmau);
+ // dot -Tpdf dothitomau.dot -o graph.pdf
+  dropgraph(g2);
+  int n1; // so phan tu cua cay
+  //bien luu tru gia tri canh khi nhap
+  int bac[MAX]={0};
+  dscanh ds1[MAX];
+  readfilePrufer("pruferdata.txt",ds1,bac,&n1);
+  printf("Prufercode:\n");
+  prtprufercode(bac,ds1,n1+1);
+  return 0;
 }
